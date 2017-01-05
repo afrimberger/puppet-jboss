a73286067f9c07da72c75db9f03252fef16fd04e
André Frimberger <andre@frimberger.de>
2017-01-05 18:52:18 +0100

    Fix registering of multiple layers in layers.conf

--- manifests/internal/module/registerlayer.pp
+++ manifests/internal/module/registerlayer.pp
@@ -12,8 +12,11 @@ define jboss::internal::module::registerlayer (
   }
 
   if (!defined(Exec["jboss::module::layer::${layer}"])) {
+    $layers_file = "${jboss::home}/modules/layers.conf"
+    $tmp_file    = "${jboss::home}/modules/layers.conf.tmp"
+
     exec { "jboss::module::layer::${layer}":
-      command => "awk -F'=' 'BEGIN {ins = 0} /^layers=/ { ins = ins + 1; print \$1=${layer},\$2 } END {if(ins == 0) print \"layers=${layer},base\"}' > ${jboss::home}/modules/layers.conf",
+      command => "awk -F'=' 'BEGIN {ins = 0} /^layers=/ { ins = ins + 1; print \$1=${layer},\$2 } END {if(ins == 0) print \"layers=${layer},base\"}' '${layers_file}' > '${tmp_file}' && mv '${tmp_file}' '${layers_file}'",
       unless  => "egrep -e '^layers=.*${layer}.*' ${jboss::home}/modules/layers.conf",
       path    => $jboss::internal::params::syspath,
       user    => $jboss::jboss_user,
